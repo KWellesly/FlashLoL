cf5ae3623b0d63505b53398b9c5d298f05a4230b
Dennis Li <dli357@gatech.edu>
2017-01-02 02:54:46 -0500

    Incorporated Kevin's recommended build code

--- refreshJsonMiniData.py
+++ refreshJsonMiniData.py
@@ -1,42 +1,60 @@
 import json
+import urllib.request
 
-with open('staticChampData.json', 'r') as inputFile:
-    data = json.load(inputFile)
-    champions = {}
-    for keys in data['keys']:
-        champions[data['keys'][keys]] = {}
+#Retrieve JSON static data
+sCDSite = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=all&api_key=RGAPI-f5b88a22-b760-4ea8-8d62-2127a752d3c3"
+staticChampionData = json.loads(urllib.request.urlopen(sCDSite).read().decode())
+sIDSite = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=all&api_key=RGAPI-f5b88a22-b760-4ea8-8d62-2127a752d3c3"
+staticItemData = json.loads(urllib.request.urlopen(sIDSite).read().decode())
 
-    #Refresh key and name set and champion list
-    keyToName = {}
-    nameToKey = {}
-    lowerKeyToName = {}
-    champList = []
-    championKeysAndNames = {'keyToName' : keyToName, 'nameToKey' : nameToKey, 'lowerKeyToName' : lowerKeyToName}
-    for keys in data['keys']:
-        key = data['keys'][keys]
-        name = data['data'][data['keys'][keys]]['name']
-        keyToName[key] = name
-        nameToKey[name] = key
-        lowerKeyToName[key.lower()] = name
-        champList.append(name.lower())
-    with open('championList.json', 'w') as nameFile:
-        json.dump(champList, nameFile)
+champions = {}
+for keys in staticChampionData['keys']:
+    champions[staticChampionData['keys'][keys]] = {}
 
-    #Refresh champion images (icon)
-    for names in data['data']:
-        champions[names]['icon'] = data['data'][names]['image']['full']
+#Refresh key and name set and champion list
+keyToName = {}
+nameToKey = {}
+lowerKeyToName = {}
+champList = []
+championKeysAndNames = {'keyToName' : keyToName, 'nameToKey' : nameToKey, 'lowerKeyToName' : lowerKeyToName}
+for keys in staticChampionData['keys']:
+    key = staticChampionData['keys'][keys]
+    name = staticChampionData['data'][staticChampionData['keys'][keys]]['name']
+    keyToName[key] = name
+    nameToKey[name] = key
+    lowerKeyToName[key.lower()] = name
+    champList.append(name.lower())
+with open('championList.json', 'w') as nameFile:
+    json.dump(champList, nameFile)
 
-    #Refresh abilities (text and images)
-    for names in data['data']:
-        abilitiesList = []
-        for spells in data['data'][names]['spells']:
-            ability = {}
-            ability['image'] = spells['image']['full']
-            ability['description'] = spells['description']
-            abilitiesList.append(ability)
-        champions[names]['abilities'] = abilitiesList
+#Refresh champion images (icon)
+for names in staticChampionData['data']:
+    champions[names]['icon'] = staticChampionData['data'][names]['image']['full']
 
-    #Compile data into one file
-    netData = {'championKeysAndNames' : championKeysAndNames, 'data' : champions, 'version' : data['version']}
-    with open('championPageData.json', 'w') as outFile:
-        json.dump(netData, outFile)
+#Refresh abilities (text and images)
+for names in staticChampionData['data']:
+    abilitiesList = []
+    for spells in staticChampionData['data'][names]['spells']:
+        ability = {}
+        ability['image'] = spells['image']['full']
+        ability['description'] = spells['description']
+        abilitiesList.append(ability)
+    champions[names]['abilities'] = abilitiesList
+
+#Refresh Recommended Build data
+for names in staticChampionData['data']:
+    itemization = {}
+    for each in (staticChampionData['data'][names]['recommended']):
+        if each['map'] == "SR":
+            for eachBuildType in each['blocks']:
+                if eachBuildType['type'] != "siegeOffense" and eachBuildType['type'] != "siegeDefense":
+                    recommendedBuild = []
+                    for eachItem in eachBuildType['items']:
+                        recommendedBuild.append({'id' : eachItem['id'], 'count' : eachItem['count'], 'image' : staticItemData['data'][str(eachItem['id'])]['image']['full']})
+                    itemization[eachBuildType['type']] = recommendedBuild
+    champions[names]['recommended'] = itemization;
+
+#Compile data into one file
+netData = {'championKeysAndNames' : championKeysAndNames, 'data' : champions, 'version' : staticChampionData['version']}
+with open('championPageData.json', 'w') as outFile:
+    json.dump(netData, outFile)
